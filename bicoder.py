1a4bacf733348f89dead9dcb5f933ed6aa286a72
YourAnonName <242625135+anon-student-GRA4152@users.noreply.github.com>
2025-11-13 02:48:25 +0100

    Implemented some small ideas I noted through the development of the main chunk (active debugging + some input defaults when different for bw and color). Fixed some bugs.get_network_config still missing implementation

--- bicoder.py
+++ bicoder.py
@@ -58,31 +58,37 @@ instance variables decoder
     color:
     - target_shape=(4,4,128)
     - channel_out=3
-    
-    
-then you have the data (x) ur passing in for traing but that's not instnace variable, that'll be arg for the method running th neural network actually
+
 '''
 
 class BiCoder(layers.Layer):
 
     # setting Rogelio's values for the neural network as default
-    def __init__(self, img_type, latent_dim, units = 400, activation = 'relu', *, 
-                kernel_size = 3, strides = 2, filters = 32  # color specific
-                ): 
+    def __init__(self, img_type, latent_dim = None, units = None, activation = 'relu',  # input params for both bw and color
+                *, kernel_size = 3, strides = 2, filters = 32):                         # optional input params specifically for color
+
+        # layers.Layer constructor so that BiCoder can inherit from there
+        super().__init__()
+
+        # active debugging -> input check that type is only 'bw' or 'color'
+        possible_img_types = ['bw', 'color']
+        assert img_type in possible_img_types, 'Image type you have inputted is not valid. It must be one of the following options: {}'.format(possible_img_types)
         
-        # add input check that type is only 'bw' or 'color'
-
-        # all instance variables def here
         self._img_type = img_type
-        self._latent_dim = latent_dim       # could add default for bw vs color with ifs?
         self._activation = activation
-        self._units = units         # could change that default for bw (400) given here and default for color = None so that it can be later assigned with the calc in decoder?
 
-        # the last 3 params are expected to be passed only for img_type = color so make them instance varibales only in that case, otherwise disregard
-        if self._img_type == 'color':
+        # set bw vs color specific defaults + the last 3 params expected only for color so initialize instance varibales only in that case, otherwise disregard
+        if self._img_type == 'bw':
+            self._latent_dim = 20 if latent_dim is None else latent_dim
+            self._units = 400 if units is None else units
+
+        elif self._img_type == 'color':
+            self._latent_dim = 50 if latent_dim is None else latent_dim
+            self._units = units
             self._kernel_size = kernel_size
             self._strides =  strides
             self._filters = filters
+
 
     # the neural network should be made depending on type
     def make_neural_network(self):
@@ -108,18 +114,23 @@ class BiCoder(layers.Layer):
 
     def get_network_configuration(self):
         pass
-    
+
 
 class Encoder(BiCoder):
-    def __init__(self, img_type, latent_dim, input_shape, units = 400, activation = 'relu', *, 
-                kernel_size = 3, strides = 2, filters = 32):
+    def __init__(self, img_type, latent_dim = None, units = None, activation = 'relu',  # BiCoder input params
+                input_shape = None,                                                     # extra input param for Encoder
+                *, kernel_size = 3, strides = 2, filters = 32):                         # optional BiCoder input params only for color
         
         # def the superclass inistance variables
         super().__init__(img_type, latent_dim, units, activation,
                         kernel_size = kernel_size, strides = strides, filters = filters)
 
-        # def all extra instance variables
-        self._input_shape = input_shape      # could add default for bw vs color with ifs?
+        # def extra encoder instance variable and set default based on img_type
+        if self._img_type == 'bw':
+            self._input_shape = (28*28,) if input_shape is None else input_shape
+        elif self._img_type == 'color':
+            self._input_shape = (28,28,3) if input_shape is None else input_shape
+            
 
     def _make_neural_network_BW(self):
         encoder_BW = Sequential(
@@ -150,13 +161,14 @@ class Encoder(BiCoder):
         return encoder_COLOR
 
     def get_network_output(self, x):
-        out = super.make_neural_network(x)
+        encoder = super().make_neural_network()
+        out = encoder(x)
         mu = out[:,:self._latent_dim]
         log_var = out[:,self._latent_dim:]
 
         return mu, log_var
 
-    def calculate_z(self, log_var, mu):
+    def calculate_z(self, mu, log_var):
         std = tf.math.exp(0.5*log_var)
         eps = tf.random.normal(mu.shape)
         z = mu + eps*std
@@ -165,15 +177,15 @@ class Encoder(BiCoder):
     # add the encoder specific params
     def get_network_configuration(self):
         pass
-    
 
-class Decoder(BiCoder):
-    def __init__(self, img_type, latent_dim, units =  400, activation = 'relu',   # Bicoder params
-                output_dim = 28*28, # bw extra decoder param with default
-                target_shape = (4,4,128), channel_out = 3, *,    # color extra decoder params with default
-                kernel_size = 3, strides = 2, filters = 32):
         
-        # def the superclass inistance variables
+class Decoder(BiCoder):
+    def __init__(self, img_type, latent_dim = None, units = None, activation = 'relu',   # BiCoder input params
+                output_dim = 28*28,                                                      # bw extra Decoder param with default
+                target_shape = (4,4,128), channel_out = 3,                               # color extra Decoder params with defaults
+                *, kernel_size = 3, strides = 2, filters = 32):                          # optional BiCoder input params only for color
+        
+        # def the superclass instance variables
         super().__init__(img_type, latent_dim,  units, activation,
                         kernel_size = kernel_size, strides = strides, filters = filters)
 
@@ -200,7 +212,7 @@ class Decoder(BiCoder):
 
     def _make_neural_network_COLOR(self):
 
-        # change the units instance varoable that was initialized in superclass at BW default
+        # change the units instance varoable that was initialized in superclass as None
         self._units = np.prod(self._target_shape)
 
         decoder_COLOR = Sequential(
@@ -223,7 +235,8 @@ class Decoder(BiCoder):
         return decoder_COLOR
 
     def get_network_output(self, x):
-        mu = super.make_neural_network(x)
+        decoder = super().make_neural_network()
+        mu = decoder(x)
         return mu
 
     # sample random x from the posterior here while just get_network_output returns the expectation -> mean (I think that's what Rogelio said?) -> test in VAE downstream tasks
@@ -236,3 +249,9 @@ class Decoder(BiCoder):
     # add the decoder specific params
     def get_network_configuration(self):
         pass
+
+
+
+# bw_encoder = Encoder('bw')
+# mu, log_var = bw_encoder.get_network_output(tf.random.normal((4, 28*28)) )
+# print(bw_encoder.calculate_z(mu, log_var))
