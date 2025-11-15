0146b0ed9469f6fd626039b48a25e4028360f257
YourAnonName <242625135+anon-student-GRA4152@users.noreply.github.com>
2025-11-15 23:28:16 +0100

    Added timestamp and a docstring. Final version even though it still logs only some weird tensorflow object as loss.

--- training_logging.py
+++ training_logging.py
@@ -1,12 +1,18 @@
-'''
-Make custom made dceorator to log the training loss from VAE in a separte logging file
+from datetime import datetime
 
-Maybe add time stamps?
-
-Not working great cause there's alreayd the tf decorator which messing up the training...
-'''
 
 class LogTraining:
+
+    ''' 
+    My own dectorator that logs the training steps of a VAE model. 
+    It records timestamp when the train method of VAE was called
+    and the resulting training loss. Unfortunatelly the training loss 
+    gets returned in some odd tensorflow format that I was not able to
+    extract the actual loss number from, so it only logs the tensorflow
+    loss object, not actual training loss. But it still showcases both
+    how to build a decorator and its purpose.
+    '''
+
     # default file where the training loss will be logged
     _logfile = 'training_loss.log'
 
@@ -21,9 +27,15 @@ class LogTraining:
         # the train func we decorating is returning loss so capture it here
         loss = self.func(*args)
 
-        # get the name of the func we decorating and add the loss
-        log_string = self.func.__name__ + " was called - loss: " + str(loss)
-        
+        # get a timestamp when the method we are decorating was called
+        timestamp = datetime.now()
+
+        # get the method name
+        func_name = self.func.__name__
+
+        # add the timestamp, method name and loss together to form the log
+        log_string = str(timestamp) + ' VAE ' + func_name + " was called - loss: " + str(loss)
+
         # Open the logfile and append
         with open (LogTraining._logfile, 'a') as opened_file:
             # Now we log to the specified logfile
