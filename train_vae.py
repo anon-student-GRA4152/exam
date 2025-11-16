618f117bf9cfac412262c6a48471819a891028fd
YourAnonName <242625135+anon-student-GRA4152@users.noreply.github.com>
2025-11-16 14:12:20 +0100

    Cleanup, added docstring. Final version

--- train_vae.py
+++ train_vae.py
@@ -1,3 +1,4 @@
+# import packages
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
@@ -7,30 +8,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.manifold import TSNE
 
+# import my own modules
+from dataloader import DataLoader
+from bw_dataloader import BW_DataLoader
+from color_dataloader import Color_DataLoader
 from vae import VAE
-from dataloader import DataLoader, BW_DataLoader, Color_DataLoader
-
-
 
 '''
-
-Could add check whether the visualizations already saved?
-Add decorator to log the training loss?
-Change the img_type input for all my classes to be mnist_bw vs mnist_color instead bw/color to be easier?
-
-- load data 
-    - make some url-dataset mapping so that I can load the correct datasets (test, train, labels) by just passing in dataset name
-- make VAE object -> color vs bw based on data
-- train VAE
-- do the downstream tasks (with argparse add that just 1 will be carried out depending on what the user inputs)
-    - generate latent space z
-    - generate new images from prior
-    - generate new images from posterior
-
-- visualize the output of the downstream tasks (using f Rogelio provided for the grid stuff)
-
-
+This is a testing file for my project. 
+It allows users to interactively in the command line pick which data set 
+they want to use (black and white images from mnist_bw or colored images
+from mnist_color). The data from the chosen dataset is downloaded and loaded
+into the tensorflow object. Then the script constructs my VAE and trains it.
+The training is done for 50 epochs by default but the user can also choose
+this parameter. Lastly the script carries out downstream tasks. The user can
+choose whether any of them should be executed with the input parameters. The
+tasks that can be done are 1) Generating and visualizing the latent space z, 
+2) Generating new images by sampling z from the prior distribution p(z), and
+3) Generating new images by sampling z from the posterior distribution q(z|x).
+The outputs of the downstream tasks are saved as pdf documents.
 '''
+# ----------------- function to plot grids -----------------------
 
 # function from Rogelio for plotting grids
 def plot_grid(images, dataset_name, downstream_task, N=10, C=10, figsize=(24., 28.)):
@@ -49,6 +47,9 @@ def plot_grid(images, dataset_name, downstream_task, N=10, C=10, figsize=(24., 2
     plt.savefig(dataset_name + '-' + downstream_task + '.pdf')
     plt.close()
 
+
+# ----------------- user input parser -----------------------
+
 # setup the parser
 parser = argparse.ArgumentParser(description = 'Training and testing my VAE')
 parser.add_argument('--dset', type = str, choices = ['mnist_bw', 'mnist_color'])
@@ -60,7 +61,7 @@ args = parser.parse_args()
 
 # ----------------- loading data -----------------------
 
-# make a mapping for the datasets and the urls needed for their testing, training, label data, plus add bw vs color as img_type
+# make a mapping for the datasets and the urls needed for their testing, training, label data
 # dataset name = key of the main dict, then smaller dict inside saying what each urls is
 datasets_urls = {'mnist_bw' : {
                                 'train' : 'https://www.dropbox.com/scl/fi/fjye8km5530t9981ulrll/mnist_bw.npy?rlkey=ou7nt8t88wx1z38nodjjx6lch&st=5swdpnbr&dl=0',
@@ -82,15 +83,19 @@ urls = datasets_urls[args.dset]
 if img_type == 'bw':
     data_loader = BW_DataLoader() 
 elif img_type == 'color':
-    data_loader = Color_DataLoader('m0')   # could add this as user input param for color or just varoable on top of this file idk yet
+    data_loader = Color_DataLoader('m2')
 
 # construct data loader for labels
 labels_data_loader = DataLoader()
 
-# load train, test data and labels
-x_train = data_loader.load_data(urls['train'])
-x_test = data_loader.load_data(urls['test'])
-labels = labels_data_loader.load_data(urls['labels'])
+# load train, test data and labels, with active debugging in case download or loading fails
+try:
+    x_train = data_loader.load_data(urls['train'])
+    x_test = data_loader.load_data(urls['test'])
+    labels = labels_data_loader.load_data(urls['labels'])
+    print('Data from {} dataset has been sucesfully loaded.'.format(args.dset))
+except:
+    print('Error loading the {} data.'.format(args.dset))
 
 # -------- VAE construction and training -------------------
 
@@ -105,12 +110,12 @@ for epoch in range(args.epochs):
     for batch_x in x_train.batch(batch_size):
         vae.train(batch_x, optimizer)
 
-# ---------- downstream tasks based on what the user picked: ------------
+# ---------- downstream tasks based on which ones the user picked ------------
 
 # 2. Generate and visualize the latent space z
 if args.visualize_latent:
     
-    # fixing the Inputs to a layer should be tensors error
+    # fixing the 'Inputs to a layer should be tensors' error
     x_test_array = np.array(list(x_test.as_numpy_iterator()))
 
     # generate z
@@ -126,40 +131,37 @@ if args.visualize_latent:
     plt.savefig(args.dset + '-generate_latent_space_z' + '.pdf')
     plt.close()
 
-
-
+# ----------------------------------------------------------
 # 3. Generate new images sampling z from the prior p(z)
 if args.generate_from_prior:
 
-    # as a default we sample the mu of x for clearer pictures (random_sampling = False) and 100 images which is the default grid dimensions from Rogelio
+    # as a default we sample the expectation (mu of x) for clearer pictures (random_sampling = False) 
+    # and 100 images which is the default grid dimensions from Rogelio
     x_hat = vae.generate_new_images_from_prior(number_of_images = 100, random_sampling = False)
 
     # need to use the clipping as Rogelio said
     img = tf.clip_by_value(255*x_hat, clip_value_min=0, clip_value_max=255).numpy().astype(np.uint8)
 
-    # fixing the shape error for bw cause we have flattened it to be only vector and now need to make it a 28*28 picture again
+    # fixing the shape error for bw, we have flattened it to be only a vector and now need to make it 28*28 again
     if img_type == 'bw':
         img = img.reshape(-1, 28, 28)
 
     plot_grid(img, args.dset, 'prior')
 
-
-
+# -----------------------------------------------------------
 # 4. Generate new images sampling z from the posterior q(z|x)
 if args.generate_from_posterior:
-
-    # maybe add generating only 100 images cause that's what I'm gonna plot anyways?
 
     # fixing the Inputs to a layer should be tensors error
     x_test_array = np.array(list(x_test.as_numpy_iterator()))
 
-    # as a default we sample the mu of x for clearer pictures (random_sampling = False)
+    # as a default we sample the expectation (mu of x) for clearer pictures (random_sampling = False) 
     x_hat = vae.generate_new_images_from_posterior(x_test_array, random_sampling = False)
 
     # need to use the clipping as Rogelio said
     img = tf.clip_by_value(255*x_hat, clip_value_min=0, clip_value_max=255).numpy().astype(np.uint8)
 
-    # fixing the shape error for bw cause we have flattened it to be only vector and now need to make it a 28*28 picture again
+    # fixing the shape error for bw, we have flattened it to be only a vector and now need to make it 28*28 again
     if img_type == 'bw':
         img = img.reshape(-1, 28, 28)
     
